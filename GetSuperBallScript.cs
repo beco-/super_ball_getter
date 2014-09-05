951e38fa782ab0c8d5aa7e53a344dd88f0f6c63c
beco- <beco-tan@beco-tan.jp>
2014-09-05 23:00:50 +0900

    Source codes of game "SUPER BALL GETTER!!" by beco-.

---
+++ GetSuperBallScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetSuperBallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		GameObject obj = GameObject.Find("GameSystemBox");
+		GameSystemScript gamesystemscript = (GameSystemScript)(obj.GetComponent("GameSystemScript"));
+
+		if(collider.gameObject.name == ("unitychan")){
+			// Delete gotten SuperBall.
+			Destroy(gameObject);
+			
+			if(gameObject.name == ("SuperBallGoal")){
+				gamesystemscript.superballCounter += 10;
+				gamesystemscript.step = GameSystemScript.STEP.GOAL;
+			}
+			else{
+				gamesystemscript.superballCounter++;
+			}
+		}
+	}
+}
---
+++ TitleScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+	private AudioSource seGet;
+	private AudioSource seSuperballgetter01;
+
+	private bool pushFlg = false;
+	private float systemTimer = 10000.0f;
+	private float textTimer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource[] audioSources = GetComponents<AudioSource>();
+		seGet = audioSources[0];
+		seSuperballgetter01 = audioSources[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DisplayPushEnterKey();
+
+		if(Input.GetKeyDown(KeyCode.Return) && pushFlg == false){
+			seGet.PlayOneShot(seGet.clip);
+			systemTimer = -3.8f; // Wait for 3.8 seconds before going to game stage.
+			pushFlg = true;
+		}
+		if((systemTimer > 0.0f) && (pushFlg == true)){
+			Application.LoadLevel("Stage1");
+		}
+		systemTimer += Time.deltaTime;
+	}
+
+	void DisplayPushEnterKey(){
+		GameObject pushEnterKeyText = GameObject.Find("PushEnterKeyText");
+		textTimer += Time.deltaTime;
+
+		// Blink the text.
+		if(textTimer < 1.0f){
+			pushEnterKeyText.guiText.text = "Push Enter Key";
+		}
+		else if(textTimer >= 1.0){
+			pushEnterKeyText.guiText.text = null;
+			
+			if(textTimer >= 1.5f){
+				textTimer = 0.0f; // Reset timer.
+			}
+		}
+	}
+}
